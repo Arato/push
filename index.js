7759ede95e8f1d04daaa5ffb3847cf6f2b23c108
Pierre BARON <prbaron22@gmail.com>
2015-03-18 21:27:02 +0100

    added console.log

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     socket.on('php.alert.created', function (response) {
+        console.log('php.alert.created', JSON.stringify(response));
         io.emit('alert.created', response);
     });
     socket.on('php.alert.updated', function (response) {
+        console.log('php.alert.updated', JSON.stringify(response));
         io.emit('alert.updated', response);
     });
     socket.on('php.alert.deleted', function (response) {
+        console.log('php.alert.deleted', JSON.stringify(response));
         io.emit('alert.deleted', response);
     });
 
